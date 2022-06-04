8da4ce9c654a1e57a796440ba4831f0ac257e7ec
carlosb93 <carlosbarcia93@gmail.com>
2022-06-04 17:18:01 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// app.js
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+app.use(
+  "/css",
+  express.static(path.join(__dirname, "views/css"))
+)
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "views/js"))
+)
+app.use(
+  "/assets",
+  express.static(path.join(__dirname, "views/assets"))
+)
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/index.html"))
+})
+
+app.listen(5000, () => {
+  console.log("Listening on port " + 5000)
+})
